037f2103e8d66713dc19c5ebead623b76a2831d0
Franchesca707 <natukodraws@gmail.com>
2026-09-08 22:01:10 -0700

    First portfolio upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/* ==================================
+   MOBILE MENU
+================================== */
+
+function toggleMenu() {
+
+    const menu = document.querySelector(".nav-links");
+
+    menu.classList.toggle("show");
+
+}
+
+
+/* ==================================
+   CLOSE MENU AFTER CLICKING
+================================== */
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        document
+            .querySelector(".nav-links")
+            .classList.remove("show");
+
+    });
+
+});
